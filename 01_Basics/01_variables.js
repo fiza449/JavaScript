ab388ca5654f82063b10bddbdc4d2aa8ef09db1d
fiza449 <fizaansari667789@gmail.com>
2025-06-16 07:17:46 +0000

    Variables

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,10 +1,11 @@
 const accountId = 37473;
 let accountEmail = "fizaansari@google.com";
 var accountPass = "23773";
+let accountsecure; //undefined: because it is declared but no value are assigned
 
 //Let's see which variables can be re-assign
 //accountId = 76534; //NOT allowed for const
 accountEmail = "shifa@google.com";  //allowed
 accountPass = "8754"; //allowed
 
-console.table([accountId,accountEmail,accountPass])
+console.table([accountId,accountEmail,accountPass,accountsecure])
