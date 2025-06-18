3ea12af3a174ded43a1c281974356da67fd8e3e6
fiza449 <fizaansari667789@gmail.com>
2025-06-18 13:42:01 +0000

    Memory

---
+++ 01_Basics/05_Memory.js
@@ -0,0 +1,15 @@
+// Stack (Primitive), Heap (Non-Primtive)
+let myYoutubeName = "hiteshchoudharydotcom";
+let anothername = myYoutubeName
+anothername = "chaiaurcode"
+console.log(anothername)
+console.log(myYoutubeName) 
+let userOne = {
+    email: "user@google.com",
+    upi : "user@bl"
+}
+let userTwo = userOne
+userTwo.email = "fiza@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
