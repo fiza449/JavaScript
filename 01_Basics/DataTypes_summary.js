c978d07c8aa3b695248a773a7c481699a7da969c
fiza449 <fizaansari667789@gmail.com>
2025-06-18 13:10:23 +0000

    DataSummary

---
+++ 01_Basics/DataTypes_summary.js
@@ -0,0 +1,17 @@
+// == : Perform data conversion then compare
+// === : Doesn't change the data strictly compare
+
+ /* ********DATA TYPES**********
+    1. Primitive(immutable): store by value (changes are done in copied value)
+        Include: String, Number, Boolean, null, undefined, Symbol, BigInt
+    2. Non-Primitive(Refrence type/ Mutable): Store By Reference
+        Include: Array, Objects, Functions
+  */ 
+ const number = 100;
+ const string = "fiza";
+ let isLoggedIn = true
+ const heroes = [1,2,3,4,5]
+ let myObj = {
+    name: "fiza",
+    age: 23
+ }
