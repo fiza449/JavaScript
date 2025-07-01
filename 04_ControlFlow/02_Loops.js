f886620d447c476a9b7ad5ac368e5ba5018e35f1
fiza449 <fizaansari667789@gmail.com>
2025-07-01 04:02:07 +0000

    LoopsInJs

---
+++ 04_ControlFlow/02_Loops.js
@@ -0,0 +1,45 @@
+//for loop
+// for (let i=0;i<=10;i++){
+//     console.log(i);
+// }
+// //Nested for loop
+// for (let i = 0; i <=10; i++) {
+//     for (let j = 0; j <=i; j++) {
+//     //     console.log("Inner Loop");
+//     // }   
+//     // console.log("Outer Loop")
+// }
+
+// for loop with array
+// let myarray = [1,2,4,6,7,2,3,5,7]
+// console.log(myarray.length)
+// for (let i = 0; i < myarray.length; i++) {
+//     const element = myarray[i];
+//     console.log(element);
+// }
+
+//break and continue
+// for (let i = 0; i <=10; i++) {
+//     if(i==5){
+//         continue;
+//     }
+//     console.log(i)
+//     if(i==9)
+//     {
+//         break;
+//     }
+// }
+
+// for (let index = 1; index <=20; index++) {
+//     if(index == 5)
+//     {
+//         console.log("5 detetcted")
+//         continue;
+//     }
+//     if(index == 12){
+//         console.log("LOOP WILL EXIT")
+//         break
+//     }
+//     console.log(index)
+    
+// }
